4da83d31cdf489b84d66dc43b95d43c24dfe6168
Dev-Lucas-Silva <desenvolvedor.lucas.dev@gmail.com>
2023-08-28 20:15:25 -0300

    estilos final e responsivo do terceiro projeto

--- 2-interactive-rating-component-main/js/script-fetch.js
+++ 2-interactive-rating-component-main/js/script-fetch.js
@@ -8,23 +8,23 @@ function formularioEnviado(resposta) {
     <h1>Thank you!</h1>
     <p>We appreciate you taking the time to give a rating. If you ever need more support, don´t hesitate to get in touch!</p></div>`;
     const span = document.querySelector(".container span");
+    span.innerHTML = parametro;
+
   } else {
     conteudo.innerHTML = `<div class="container-resposta"><img src="./images/illustration-thank-you.svg" alt="">
     <p>You selected <span></span> out of 5</p>
     <h1>Thank you!</h1>
     <p>We appreciate you taking the time to give a rating. If you ever need more support, don´t hesitate to get in touch!</p></div>`;
-
   }
-  
+
   const parametros = new URLSearchParams(location.search);
-  console.log(parametros);
-  
+
   function classificacaoAtivado(parametro) {
     console.log(parametro);
     const span = document.querySelector(".container span");
     span.innerHTML = parametro;
   }
-  
+
   parametros.forEach(classificacaoAtivado);
 }
 
@@ -34,10 +34,15 @@ function enviarFormulario(event) {
   botao.disabled = true;
 
   const data = new FormData(formulario);
+  const datajson = Object.fromEntries(data.entries());
+ console.log(datajson);
 
-  fetch("./enviar.php", {
-    method: "post",
-    body: data,
+  fetch("./data.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(datajson),
   }).then(formularioEnviado);
 }
 
--- 2-interactive-rating-component-main/js/script.js
+++ 2-interactive-rating-component-main/js/script.js
@@ -1,8 +1,24 @@
-const form = document.getElementById("form");
+const formulario = document.querySelector("form");
+const conteudo = document.querySelector(".container");
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    const number = document.querySelector('input[name="rating"]:checked');
-    localStorage.setItem("rating",number.value);
-    window.location.href="resposta.html";
-});
+function formularioEnviado() {
+  conteudo.innerHTML = `<div class="container-resposta"><img src="./images/illustration-thank-you.svg" alt="">
+    <p>You selected <span></span> out of 5</p>
+    <h1>Thank you!</h1>
+    <p>We appreciate you taking the time to give a rating. If you ever need more support, don´t hesitate to get in touch!</p></div>`;
+
+  const data = new FormData(formulario);
+  const datajson = Object.fromEntries(data.entries());
+
+  const span = document.querySelector(".container span");
+  span.innerHTML = datajson.rating;
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = true;
+  formularioEnviado();
+}
+
+formulario.addEventListener("submit", enviarFormulario);
