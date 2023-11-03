e96265aec2932ebbbf43bfe50c9fc8beda9c4134
Dev-Lucas-Silva <desenvolvedor.lucas.dev@gmail.com>
2023-11-03 18:54:30 -0300

    validacao de form js componente 5

--- 5-base-apparel-coming-soon/script.js
+++ 5-base-apparel-coming-soon/script.js
@@ -1,3 +1,24 @@
-const button = document.querySelector('.content button');
+const form = document.getElementById("email");
+const erro = document.querySelector(".erro");
 
-console.log(button);
+function handleChangeValid(event) {
+  const target = event.target;
+  if (target.checkValidity()) {
+    target.classList.remove("invalido");
+    erro.innerText = "";
+    console.log("target");
+  }
+}
+
+function handleChange(event) {
+  const target = event.target;
+  if (!target.checkValidity()) {
+    target.classList.add("invalido");
+    target.setCustomValidity("Please provide a valid email");
+    erro.innerText = target.validationMessage;
+    console.log("tcdcdcdget");
+  }
+}
+
+form.addEventListener("change", handleChange);
+form.addEventListener("change", handleChangeValid);
